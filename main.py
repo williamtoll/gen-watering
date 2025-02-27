8a6ef7044b0010597606290f280366e68136f634
William Toledo <william.toledo@verusen.com>
2025-02-27 13:37:54 -0300

    duration min

--- main.py
+++
@@ -1,8 +0,0 @@
-from dateutil.rrule import *
-from dateutil.parser import *
-from datetime import *
-
-
-print(list(rrule(DAILY, count=10, dtstart=parse("19970902T090000"))))
-
-list(rrule(DAILY,dtstart=parse("19970902T090000"),until=parse("19971224T000000")))
